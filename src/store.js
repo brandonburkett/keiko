16c919b1f610ba91ebf9cb0d93a9b869c1e365e2
Brandon Burkett <brandon@brandon-san.com>
2019-10-03 17:01:31 -0500

    kata model

--- src/store.js
+++ src/store.js
@@ -3,15 +3,109 @@ import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
+/**
+ * Kata Module
+ */
+const kata = {
   state: {
-    kata: {
-      list: [],
-      active: null,
-      previous: null,
-      completed: 0,
+    list: [
+      {
+        name: 'Mae',
+        kanji: '前',
+        meaning: 'Front',
+        seriesKey: 'shoden',
+        order: 1,
+      },
+      {
+        name: 'Migi',
+        kanji: '右',
+        meaning: 'Right',
+        seriesKey: 'shoden',
+        order: 2,
+      },
+      {
+        name: 'Hidari',
+        kanji: '左',
+        meaning: 'Left',
+        seriesKey: 'shoden',
+        order: 3,
+      },
+      {
+        name: 'Ushiro',
+        kanji: '後',
+        meaning: 'Rear / behind',
+        seriesKey: 'shoden',
+        order: 4,
+      },
+      {
+        name: 'Yaegaki',
+        kanji: '八重垣',
+        meaning: 'Barriers within barriers',
+        seriesKey: 'shoden',
+        order: 5,
+      },
+      {
+        name: 'Ukenagashi',
+        kanji: '受流',
+        meaning: 'Flowing block / receive and deflect',
+        seriesKey: 'shoden',
+        order: 6,
+      },
+      {
+        name: 'Kaishaku',
+        kanji: '介錯',
+        meaning: 'Second / seppuku assistan',
+        seriesKey: 'shoden',
+        order: 7,
+      },
+      {
+        name: 'Tsukekomi',
+        kanji: '附込',
+        meaning: 'Seize opportunity / pursuit',
+        seriesKey: 'shoden',
+        order: 8,
+      },
+      {
+        name: 'Tsukikage',
+        kanji: '月影',
+        meaning: 'Moon shadow',
+        seriesKey: 'shoden',
+        order: 9,
+      },
+      {
+        name: 'Oikaze',
+        kanji: '追風',
+        meaning: 'Chasing wind / tailwind',
+        seriesKey: 'shoden',
+        order: 10,
+      },
+      {
+        name: 'Nukiuchi',
+        kanji: '抜打',
+        meaning: 'Draw and strike',
+        seriesKey: 'shoden',
+        order: 11,
+      },
+    ],
+    series: {
+      shoden: {
+        name: 'Shoden Waza',
+        kanji: '初傳技',
+      },
+    },
+    active: null,
+    previous: null,
+    completed: 0,
+  },
+  getters: {
+    getSeries: state => key => {
+      return state.series[key];
     },
   },
-  mutations: {},
-  actions: {},
+};
+
+export default new Vuex.Store({
+  modules: {
+    kata,
+  },
 });
