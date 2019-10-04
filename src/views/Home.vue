dba13d7874e8116392c6be08ecb28da55437e3f6
Brandon Burkett <brandon@brandon-san.com>
2019-10-04 18:50:48 -0500

    circleci build

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,9 +2,9 @@
   <PageFull>
     <div class="flex flex-col h-full w-100 m-0 sm:w-6/12 sm:mx-auto">
       <div>
-        <h1 class="text-3xl font-bold text-center">Always Train!</h1>
+        <h1 class="text-6xl font-bold text-center">Keiko Keiko Keiko</h1>
         <p class="text-justify my-4">
-          Welcome to Keiko, the app to help training MJER kata. Click begin to work through each
+          Welcome to Keiko, the app to help train MJER kata. Click begin to work through each
           technique, chosen at random, to test your knowledge and push yourself forward!
         </p>
       </div>
